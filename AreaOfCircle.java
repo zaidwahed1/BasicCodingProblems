f8beadfc43b2d11a21e0202013f1a36e154c394a
zaidwahed1 <51842376+zaidwahed1@users.noreply.github.com>
2019-06-14 22:28:35 -0400

    Add files via upload

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-class AreaOfCircle 
+import java.util.Scanner;			//Command to import the scanner class for taking in input
+class AreaOfCircle 					//Name of your class 
 {
-   public static void main(String args[]) 
+   public static void main(String args[]) //declaration of main class
     {          
-      Scanner s= new Scanner(System.in);
-         System.out.println("Enter the radius:");
-         double r= s.nextDouble();
-         double  area=(22*r*r)/7 ;
-         System.out.println("Area of Circle is: " + area);      
+      Scanner s= new Scanner(System.in);	//Declaring scanner variable for taking in input
+         System.out.println("Enter the radius:");	//Getting values from user
+         double r= s.nextDouble();					//Declared as double, so that decimals are also included
+         double  area=(22*r*r)/7 ;					//Declared as double, so that decimal values are not rounded.
+         System.out.println("Area of Circle is: " + area);      //Display the area
    }
 }
